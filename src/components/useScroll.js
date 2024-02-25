c78beae19eb174d423e76aa84481ab14d4e81bd4
Bilal Ahmad <you@example.com>
2024-02-26 04:13:42 +0500

    fixing bug in useScroll file

--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -1,19 +1,4 @@
-// import React from "react";
-// import { useAnimation } from "framer-motion";
-// import { useInView } from "react-intersection-observer";
-
-// export const useScroll = () => {
-//   const controls = useAnimation();
-//   const [element, view] = useInView();
-//   if (view) {
-//     controls.start("show");
-//   } else {
-//     controls.start("hidden");
-//   }
-//   return [element, controls];
-// };
-
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
